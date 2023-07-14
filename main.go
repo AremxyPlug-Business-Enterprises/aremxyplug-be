ad3ba10b54067a78c473343a7d27deb1057b6e78
Ayanda Jeremiah Ayoku <Olumaintain619@gmail.com>
2023-07-14 21:58:32 +0100

    cors config

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func corsMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization", "Origin", "Accept", "Content-Type", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Origin, Accept, Content-Type, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers")
         c.Writer.Header().Set("Access-control-Allow-Credentials", "true")
 
         if c.Request.Method == "OPTIONS" {
