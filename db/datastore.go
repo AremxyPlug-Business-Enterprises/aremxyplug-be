4837c61612119728e042b0b20e83b18d63a03685
Ayanda Jeremiah Ayoku <Olumaintain619@gmail.com>
2023-11-06 11:31:04 +0100

    username auth

--- db/datastore.go
+++ db/datastore.go
@@ -9,6 +9,8 @@ type DataStore interface {
 type AremxyStore interface {
 	SaveUser(user models.User) error
 	GetUserByEmail(email string) (*models.User, error)
+	GetUserByUsername(username string) (*models.User, error)
+	GetUserByUsernameOrEmail(email string, username string) (*models.User, error)
 	CreateMessage(message *models.Message) error
 	UpdateUserPassword(email string, password string) error
 	SaveDataTransaction(details interface{}) error
