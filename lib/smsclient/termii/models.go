58e5c95865615992fc16cff0eed7e4bc417e4066
Victory Agbabune <victoryotaghogho@gmail.com>
2025-02-20 12:01:12 -0500

    feat: implement custom JSON unmarshaller for verifyTokenResponse

--- lib/smsclient/termii/models.go
+++ lib/smsclient/termii/models.go
@@ -1,5 +1,10 @@
 package termii
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type smsRequest struct {
 	APIKey         string `json:"api_key"`
 	MessageType    string `json:"message_type"`
@@ -30,4 +35,37 @@ type verifyTokenResponse struct {
 	PinID    string `json:"pinId"`
 	Verified bool   `json:"verified"`
 	Msisdn   string `json:"msisdn"`
+}
+
+// UnmarshalJSON custom unmarshaller for verifyTokenResponse
+func (v *verifyTokenResponse) UnmarshalJSON(data []byte) error {
+	type Alias verifyTokenResponse
+	aux := &struct {
+		Verified interface{} `json:"verified"`
+		*Alias
+	}{
+		Alias: (*Alias)(v),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	switch value := aux.Verified.(type) {
+	case bool:
+		v.Verified = value
+	case string:
+		switch value {
+		case "Expired":
+			v.Verified = false
+
+		default:
+			return fmt.Errorf("unexpected value for string: %v", value)
+		}
+
+	default:
+		return fmt.Errorf("unexpected type for verified field: %T", value)
+	}
+
+	return nil
 }
